c741d118fb9d7e65bcc2e3dc54c383f923537333
imrahilias <siegel.moritz@gmail.com>
2021-03-22 18:29:17 +0100

    first commit: problem solved :D

---
+++ dbscan.m
@@ -0,0 +1,52 @@
+#!/bin/octave
+## This reposetory contains a simple implementation of DBSCAN algorithm using GNU OCTAVE.
+## DBSCAN is a popular clustering algorithm. It creates clusters on a spatial data depending on two parameters:
+## MinPoints: minimum number of points needed in its neighbourhood to consider it as a valid data(not noise).
+## EPS: A distance on which neighbourhood is calculated.
+## For more info:
+## https://github.com/devil1993/DBSCAN
+## https://en.wikipedia.org/wiki/DBSCAN
+## http://citeseerx.ist.psu.edu/viewdoc/download;jsessionid=1A6A7A85AF3F43BCBF66D847FEC8F8C5?doi=10.1.1.121.9220&rep=rep1&type=pdf
+function [assignments,C] = dbscan(X,minpts,EPS)
+  C = 0;
+  assignments = zeros(size(X)(1),1);
+  clustered = zeros(size(X)(1),1);
+  for i=1: size(X)(1)
+    if(clustered(i)==1)
+      continue;
+    endif
+    clustered(i)=1;
+    isneighbour = [];
+    neighbourcount = 0;
+    for j=1: size(X)(1)
+      dist = sqrt(sum((X(i,:)-X(j,:)).^2));
+      if(dist<EPS)
+        neighbourcount++;
+        isneighbour = [isneighbour j];
+      endif
+    endfor
+    if(neighbourcount<minpts)
+      continue;
+    else
+      C++;
+      assignments(i) = C;
+      for k=isneighbour
+        if(clustered(k)==0)
+          clustered(k) = 1;
+          _isneighbour = [];
+          _neighbourcount = 0;
+          for j=1: size(X)(1)
+            dist = sqrt(sum((X(k,:)-X(j,:)).^2));
+            if(dist<EPS)
+              _neighbourcount++;
+              _isneighbour = [_isneighbour j];
+            endif
+          endfor
+          if(_neighbourcount>=minpts)
+            isneighbour = [isneighbour _isneighbour];
+          endif
+        endif
+        assignments(k) = C;
+      endfor
+    endif
+  endfor
---
+++ rig.m
@@ -0,0 +1,47 @@
+#!/bin/octave
+## this function rig(p,q) finds the optimal rigid transform in 3-dimensional
+## euclidian space, using least squares and
+## single-value-decomposition. input a 3xn matrix (set of n 3d
+## positions), and the rotation matrtix r, it returns the translation
+## vector t.
+## K. S. Arun, T. S. Huang and S. D. Blostein, "Least-Squares Fitting of
+## Two 3-D Point Sets," in IEEE Transactions on Pattern Analysis and
+## Machine Intelligence, vol. PAMI-9, no. 5, pp. 698-700, Sept. 1987,
+## doi: 10.1109/TPAMI.1987.4767965.
+## moritz siegel @ 210322
+function [ r, t, s ] = rig( p, q )
+  assert( nargin == 2 && size( p ) == size( q ), ...
+          'need 2 identical input matrices' );
+  assert( size( p, 1 ) == 3, 'input matrix p must be 3xn' );
+  assert( size( q, 1 ) == 3, 'input matrix q must be 3xn' );
+  n = size( p, 2 );
+  assert( n > 2, 'need at least 2 points' );
+  
+  ## find centroids & shift there
+  centroid_p = mean( p, 2 );
+  centroid_q = mean( q, 2 );
+  p_shifted = p - repmat( centroid_p, 1, n );
+  q_shifted = q - repmat( centroid_q, 1, n );
+  
+  ## covariance (?) matrix
+  h = p_shifted * q_shifted';
+  
+  ## singular value decomposition
+  [ u, s, v ] = svd( h );
+  r = v * u';
+  
+  if ( det( r ) < 0 )
+    printf( "warning: det(r) < 0" );
+    if ( any( s(:) ) < 0 )
+      printf( "found reflection, correcting." );
+      v(:,3) = -v(:,3);
+      r = v*u';
+    else
+      printf( "error: single-value-decomposition failed! \
+provided data seems is too noisy for least-squares." );  
+      exit(1)
+    endif
+  endif
+
+  t = centroid_q - r * centroid_p;
+end
